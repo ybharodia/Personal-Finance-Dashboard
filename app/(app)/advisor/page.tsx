1581dd2f1455a89640a84f580a570317a0f08e20
Yash Bharodia <yash@Yashs-MacBook-Air-2.local>
2026-03-20 09:47:01 -0400

    Remove AI advisor functionality

--- app/(app)/advisor/page.tsx
+++
@@ -1,9 +0,0 @@
-import { buildAdvisorBriefing } from "@/lib/advisor";
-import AdvisorClient from "@/components/AdvisorClient";
-
-export const dynamic = "force-dynamic";
-
-export default async function AdvisorPage() {
-  const briefing = await buildAdvisorBriefing();
-  return <AdvisorClient briefing={briefing} />;
-}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -59,15 +59,6 @@ const navItems = [
       </svg>
     ),
   },
-  {
-    label: "Advisor",
-    href: "/advisor",
-    icon: (
-      <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-3 3-3-3z" />
-      </svg>
-    ),
-  },
 ];
 
 export default function Sidebar() {
